24a91758a925da1b5927f6d3e167a0dbf25398ee
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-11 02:59:29 +0530

    comments added

--- Solution03.java
+++ Solution03.java
@@ -1,3 +1,5 @@
+// In this challenge, you must read an integer, a double, and a String from stdin, then print the values according to the instructions in the Output Format section below.
+
 import java.util.Scanner;
 
 public class Solution03 {
